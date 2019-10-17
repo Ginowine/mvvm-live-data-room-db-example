ef93ef1fbcf60cc4e5d6b4ad152b40b6fa4dfde1
ginowine <emailosahon@yahoo.com>
2019-10-17 22:17:27 +0100

    Dao and Room DB created

---
+++ app/src/main/java/Word.java
@@ -0,0 +1,18 @@
+import androidx.annotation.*;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    public String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
